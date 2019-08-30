d969c136703e5a8c8308c149cef3e0092eb7ff05
michelfigueiredojava <michelfigueiredojava@gmail.com>
2019-08-30 22:57:08 +0100

    bouncing ball

--- micro-katas/src/main/java/com/michelfigueiredo/kata/micro/bouncingballs/BouncingBall.java
+++ micro-katas/src/main/java/com/michelfigueiredo/kata/micro/bouncingballs/BouncingBall.java
@@ -1,15 +1,19 @@
 package com.michelfigueiredo.kata.micro.bouncingballs;
 
 public class BouncingBall {
-    public static int bounce(int buildingHeight, double bounce, double windowHeight) {
+    public static int bouncingBall(double h, double bounce, double window) {
+        if (h <= 0) return -1;
+        if (bounce <= 0 || bounce >= 1) return -1;
+        if (window >= h) return -1;
+
         int seenCounter;
-        double ballHeight = buildingHeight;
+        double ballHeight = h;
 
         seenCounter = 1;
-        while( (ballHeight = ballHeight * bounce) > windowHeight ){
+        while ((ballHeight = ballHeight * bounce) > window) {
             seenCounter = seenCounter + 2;
-        };
-
+        }
         return seenCounter;
+
     }
 }
--- micro-katas/src/test/java/com/michelfigueiredo/kata/micro/bouncingballs/BouncingBallTest.java
+++ micro-katas/src/test/java/com/michelfigueiredo/kata/micro/bouncingballs/BouncingBallTest.java
@@ -8,17 +8,55 @@ class BouncingBallTest {
 
     @Test
     void can_bounce_from_height_3_bounce_066_window_150() {
-        int ballSeenCount = BouncingBall.bounce( 3, 0.66, 1.5 );
+        int ballSeenCount = BouncingBall.bouncingBall( 3, 0.66, 1.5 );
 
         assertEquals( 3, ballSeenCount );
     }
 
-
     @Test
     void can_bounce_from_height_30_bounce_066_window_150() {
-        int ballSeenCount = BouncingBall.bounce( 30, 0.66, 1.5 );
+        int ballSeenCount = BouncingBall.bouncingBall( 30, 0.66, 1.5 );
 
         assertEquals( 15, ballSeenCount );
+    }
+
+
+    @Test
+    void can_bounce_from_height_30000_bounce_090_window_100() {
+        int ballSeenCount = BouncingBall.bouncingBall( 99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999D,
+            0.99,
+            0.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001D );
+
+        assertEquals( 180993, ballSeenCount );
+    }
+
+
+    @Test
+    void cant_bounce_from_invalid_height() {
+        int ballSeenCount = BouncingBall.bouncingBall( 0, 0.66, 1.5 );
+
+        assertEquals( -1, ballSeenCount );
+    }
+
+    @Test
+    void cant_bounce_from_bounce_less_than_0() {
+        int ballSeenCount = BouncingBall.bouncingBall( 3, -0.1, 1.5 );
+
+        assertEquals( -1, ballSeenCount );
+    }
+
+    @Test
+    void cant_bounce_from_bounce_greater_than_1() {
+        int ballSeenCount = BouncingBall.bouncingBall( 3, 1.1, 1.5 );
+
+        assertEquals( -1, ballSeenCount );
+    }
+
+    @Test
+    void cant_bounce_from_invalid_window_height() {
+        int ballSeenCount = BouncingBall.bouncingBall( 1, 0.66, 1 );
+
+        assertEquals( -1, ballSeenCount );
     }
 
 }
